e15d39c5224d810caa1638b2b89da1d84c3ce877
kpvt001 <kpvt001@qq.com>
2016-08-10 20:51:00 +0800

    modify get_ishadowsocks_password.py

--- get_ishadowsocks_password.py
+++ get_ishadowsocks_password.py
@@ -2,11 +2,20 @@
 
 import sys, urllib.request
 
+#defines
 class ServerInfo:
 	def __init__ (self, svr_name, svr_pwd_header):
 		self.name = svr_name
 		self.pwd_header = svr_pwd_header
 		self.password = ""
+		
+password_string_header_len = 8
+password_len = 8
+password_file_name = 'ishadowsocks_passwords.txt'
+url = "http://www.ishadowsocks.org/" #网页地址
+us_svr_info = ServerInfo('US', 'A密码:')
+hk_svr_info = ServerInfo('HK', 'B密码:')
+jp_svr_info = ServerInfo('JP', 'C密码:')
 
 def GetServerPassword(content, svr):
 	find_str_index = content.find(svr.pwd_header.encode())
@@ -30,34 +39,16 @@ def BuildResultString(svr):
 
 	return tstr
 
-#defines
-password_string_header_len = 8
-password_len = 8
-password_file_name = 'ishadowsocks_passwords.txt'
-url = "http://www.ishadowsocks.org/" #网页地址
-
-us_svr_info = ServerInfo('US', 'A密码:')
-hk_svr_info = ServerInfo('HK', 'B密码:')
-jp_svr_info = ServerInfo('JP', 'C密码:')
-
+#begin
 result = ''
-
 url_request = urllib.request.urlopen(url)
 content = url_request.read()
 
-# Get US server password
+svr_list = [us_svr_info, hk_svr_info, jp_svr_info]
 
-if GetServerPassword(content, us_svr_info):
-	result = result + BuildResultString(us_svr_info) + '\n'
-
-# Get HK server password
-
-if GetServerPassword(content, hk_svr_info):
-	result = result + BuildResultString(hk_svr_info) + '\n'
-
-# Get JP server password
-if GetServerPassword(content, jp_svr_info):
-	result = result + BuildResultString(jp_svr_info) + '\n'
+for svr_item in svr_list:
+	if GetServerPassword(content, svr_item):
+		result = result + BuildResultString(svr_item) + '\n'
 
 print(result)
 
