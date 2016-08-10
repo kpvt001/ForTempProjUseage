2c84d84a3aa9fbd45202a522b10cbbce2c7af411
kpvt001 <kpvt001@qq.com>
2016-08-10 20:32:37 +0800

    modify get_ishadowsocks_password.py

--- get_ishadowsocks_password.py
+++ get_ishadowsocks_password.py
@@ -8,20 +8,31 @@ class ServerInfo:
 		self.pwd_header = svr_pwd_header
 		self.password = ""
 
+def GetServerPassword(content, svr):
+	find_str_index = content.find(svr.pwd_header.encode())
+	start_index = find_str_index + password_string_header_len
+	end_index = start_index + password_len
 
+	t = content[start_index:end_index]
+
+	if t.decode().isdigit():
+		svr.password = t.decode()
+		return True
+	else:
+		svr.password = ""
+		return False
+
+def BuildResultString(svr):
+	if svr.name == "":
+		tstr = svr.name + ' Server password not found' + '\n'
+	else:
+		tstr = svr.name + ' Server password is: ' + svr.password
+
+	return tstr
 
 #defines
 password_string_header_len = 8
 password_len = 8
-us_password_header = 'A密码:'
-hk_password_header = 'B密码:'
-jp_password_header = 'C密码:'
-
-
-
-us_name = 'US'
-hk_name = 'HK'
-jp_name = 'JP'
 password_file_name = 'ishadowsocks_passwords.txt'
 url = "http://www.ishadowsocks.org/" #网页地址
 
@@ -35,67 +46,22 @@ url_request = urllib.request.urlopen(url)
 content = url_request.read()
 
 # Get US server password
-find_str_index = content.find(us_password_header.encode())
 
-start_index = find_str_index + password_string_header_len
-end_index = start_index + password_len
-t = content[start_index:end_index]
-
-svr_name = us_name
-if t.decode().isdigit():
-	svr_ret = svr_name + ' Server password is: ' + t.decode()
-else:
-	svr_ret = svr_name + ' Server password not found'
-result = result + svr_ret +'\n'
-print(svr_ret)
+if GetServerPassword(content, us_svr_info):
+	result = result + BuildResultString(us_svr_info) + '\n'
 
 # Get HK server password
-find_str_index = content.find(hk_password_header.encode())
 
-start_index = find_str_index + password_string_header_len
-end_index = start_index + password_len
-t = content[start_index:end_index]
-
-svr_name = hk_name
-if t.decode().isdigit():
-	svr_ret = svr_name + ' Server password is: ' + t.decode()
-else:
-	svr_ret = svr_name + ' Server password not found'
-result = result + svr_ret +'\n'
-print(svr_ret)
+if GetServerPassword(content, hk_svr_info):
+	result = result + BuildResultString(hk_svr_info) + '\n'
 
 # Get JP server password
-find_str_index = content.find(jp_password_header.encode())
+if GetServerPassword(content, jp_svr_info):
+	result = result + BuildResultString(jp_svr_info) + '\n'
 
-start_index = find_str_index + password_string_header_len
-end_index = start_index + password_len
-t = content[start_index:end_index]
-
-svr_name = jp_name
-if t.decode().isdigit():
-	svr_ret = svr_name + ' Server password is: ' + t.decode()
-else:
-	svr_ret = svr_name + ' Server password not found'
-result = result + svr_ret +'\n'
-print(svr_ret)
-
+print(result)
 
 #Write to file
 psw_file = open(password_file_name, 'w+')
 psw_file.write(result)
 psw_file.close()
-
-
-def GetServerPassword(content, svr):
-	find_str_index = content.find(svr.pwd_header.encode())
-	start_index = find_str_index + password_string_header_len
-	end_index = start_index + password_len
-
-	t = content[start_index:end_index]
-
-	if t.decode().isdigit():
-		svr.password = t.decode()
-	else:
-		svr.password = ""
-		
-	return t.decode()
